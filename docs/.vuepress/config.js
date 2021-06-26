7f2a454cd6b96b0b0143272b3e33f292e6287f20
tony <nitao.227@gmail.com>
2021-06-26 11:49:52 +0800

    up

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -13,7 +13,7 @@ module.exports = {
         // 博客配置
         type: 'blog',
         author: '逸山',
-        authorAvatar: '/avatar.png',
+        authorAvatar: '/avatar.jpg',
         subSidebar: 'auto', //在所有页面中启用自动生成子侧边栏，原 sidebar 仍然兼容
         nav: [
             { text: '时间线', link: '/timeline/', icon: 'reco-date' }
